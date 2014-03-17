e59071acfa92b23e7dad19e845984468b95d1c07
Jason Ho <eflyjason@gmail.com>
2014-03-17 18:26:04 +0800

    Update Chinese To Pinyin Tools

--- chinese-to-pinyin/index.php
+++ chinese-to-pinyin/index.php
@@ -4,109 +4,27 @@ $seo = array(
 	'description' => '想要把一段文字轉換為拼音？快來試試Arefly製作的「拼音轉換器」吧！',
 	'keywords' => '拼音, 拼音轉換, 中文',
 );
+$header = array(
+	'extra_script' => array("pinyin.js"),
+);
 ?>
-<?php get_header("拼音轉換", $seo); ?>
+<?php get_header("拼音轉換", $seo, $header); ?>
 <form class="form-horizontal" action="" method="post" role="form">
-	<div class="<?php echo get_alert_class('warning'); ?>" style="padding-top: 30px;">
-<ruby><rb>注</rb><rt>Zhu</rt></ruby>
-<ruby><rb>意</rb><rt>Yi</rt></ruby>
-<ruby><rb>：</rb><rt>：</rt></ruby>
-<ruby><rb>拼</rb><rt>Pin</rt></ruby>
-<ruby><rb>音</rb><rt>Yin</rt></ruby>
-<ruby><rb>轉</rb><rt>Zhuan</rt></ruby>
-<ruby><rb>換</rb><rt>Huan</rt></ruby>
-<ruby><rb>時</rb><rt>Shi</rt></ruby>
-<ruby><rb>繁</rb><rt>Fan</rt></ruby>
-<ruby><rb>體</rb><rt>Ti</rt></ruby>
-<ruby><rb>大</rb><rt>Da</rt></ruby>
-<ruby><rb>部</rb><rt>Bu</rt></ruby>
-<ruby><rb>分</rb><rt>Fen</rt></ruby>
-<ruby><rb>時</rb><rt>Shi</rt></ruby>
-<ruby><rb>候</rb><rt>Hou</rt></ruby>
-<ruby><rb>不</rb><rt>Bu</rt></ruby>
-<ruby><rb>會</rb><rt>Hui</rt></ruby>
-<ruby><rb>『</rb><rt>『</rt></ruby>
-<ruby><rb>有</rb><rt>Zuozhe</rt></ruby>
-<ruby><rb>』</rb><rt>』</rt></ruby>
-<ruby><rb>問</rb><rt>Wen</rt></ruby>
-<ruby><rb>『</rb><rt>『</rt></ruby>
-<ruby><rb>題</rb><rt>Di</rt></ruby>
-<ruby><rb>』</rb><rt>』</rt></ruby>
-<ruby><rb>，</rb><rt>，</rt></ruby>
-<ruby><rb>但</rb><rt>Dan</rt></ruby>
-<ruby><rb>是</rb><rt>Shi</rt></ruby>
-<ruby><rb>在</rb><rt>Zai</rt></ruby>
-<ruby><rb>『</rb><rt>『</rt></ruby>
-<ruby><rb>有</rb><rt>Zuozhe</rt></ruby>
-<ruby><rb>』</rb><rt>』</rt></ruby>
-<ruby><rb>些</rb><rt>Xie</rt></ruby>
-<ruby><rb>小</rb><rt>Xiao</rt></ruby>
-<ruby><rb>地</rb><rt>Di</rt></ruby>
-<ruby><rb>方</rb><rt>Fang</rt></ruby>
-<ruby><rb>就</rb><rt>Jiu</rt></ruby>
-<ruby><rb>會</rb><rt>Hui</rt></ruby>
-<ruby><rb>出</rb><rt>Chu</rt></ruby>
-<ruby><rb>問</rb><rt>Wen</rt></ruby>
-<ruby><rb>題</rb><rt>Di</rt></ruby>
-<ruby><rb>，</rb><rt>，</rt></ruby>
-<ruby><rb>例</rb><rt>Li</rt></ruby>
-<ruby><rb>如</rb><rt>Ru</rt></ruby>
-<ruby><rb>：</rb><rt>：</rt></ruby>
-<ruby><rb>『</rb><rt>『</rt></ruby>
-<ruby><rb>說</rb><rt>Shui</rt></ruby>
-<ruby><rb>』</rb><rt>』</rt></ruby>
-<ruby><rb>。</rb><rt>。</rt></ruby>
-<ruby><rb>。</rb><rt>。</rt></ruby>
-<ruby><rb>。</rb><rt>。</rt></ruby>
-<ruby><rb>。</rb><rt>。</rt></ruby>
-<ruby><rb>。</rb><rt>。</rt></ruby>
-<ruby><rb>简</rb><rt>Jian</rt></ruby>
-<ruby><rb>体</rb><rt>Ti</rt></ruby>
-<ruby><rb>就</rb><rt>Jiu</rt></ruby>
-<ruby><rb>一</rb><rt>Yi</rt></ruby>
-<ruby><rb>点</rb><rt>Dian</rt></ruby>
-<ruby><rb>问</rb><rt>Wen</rt></ruby>
-<ruby><rb>题</rb><rt>Ti</rt></ruby>
-<ruby><rb>都</rb><rt>Dou</rt></ruby>
-<ruby><rb>没</rb><rt>Mei</rt></ruby>
-<ruby><rb>啦</rb><rt>La</rt></ruby>
-<ruby><rb>！</rb><rt>！</rt></ruby>
-<ruby><rb>『</rb><rt>『</rt></ruby>
-<ruby><rb>所</rb><rt>Zuozhe</rt></ruby>
-<ruby><rb>』</rb><rt>』</rt></ruby>
-<ruby><rb>以</rb><rt>Yi</rt></ruby>
-<ruby><rb>大</rb><rt>Da</rt></ruby>
-<ruby><rb>家</rb><rt>Jia</rt></ruby>
-<ruby><rb>還</rb><rt>Hai</rt></ruby>
-<ruby><rb>是</rb><rt>Shi</rt></ruby>
-<ruby><rb>最</rb><rt>Zui</rt></ruby>
-<ruby><rb>好</rb><rt>Hao</rt></ruby>
-<ruby><rb>先</rb><rt>Xian</rt></ruby>
-<ruby><rb>使</rb><rt>Shi</rt></ruby>
-<ruby><rb>用</rb><rt>Yong</rt></ruby>
-<ruby><rb>本</rb><rt>Ben</rt></ruby>
-<ruby><rb>站</rb><rt>Zhan</rt></ruby>
-<ruby><rb>小</rb><rt>Xiao</rt></ruby>
-<ruby><rb>工</rb><rt>Gong</rt></ruby>
-<ruby><rb>具</rb><rt>Ju</rt></ruby>
-<ruby><rb>「</rb><rt>「</rt></ruby>
-<ruby><rb>簡</rb><rt>Jian</rt></ruby>
-<ruby><rb>繁</rb><rt>Fan</rt></ruby>
-<ruby><rb>轉</rb><rt>Zhuan</rt></ruby>
-<ruby><rb>換</rb><rt>Huan</rt></ruby>
-<ruby><rb>」</rb><rt>」</rt></ruby>
-<ruby><rb>來</rb><rt>Lai</rt></ruby>
-<ruby><rb>轉</rb><rt>Zhuan</rt></ruby>
-<ruby><rb>換</rb><rt>Huan</rt></ruby>
-<ruby><rb>至</rb><rt>Zhi</rt></ruby>
-<ruby><rb>簡</rb><rt>Jian</rt></ruby>
-<ruby><rb>體</rb><rt>Ti</rt></ruby>
+	<div class="text-center <?php echo get_alert_class('info'); ?>">
+		使用方法：在下方文本框輸入中文（簡繁皆可），然後點擊要「轉換」即可
 	</div>
 	<div class="form-group">
 		<div class="col-sm-10 centerd">
-			<textarea class="form-control" name="word" rows="10" cols="86" onkeyup="<?php echo use_ajax('get_pinyin'); ?>"><?php echo $_POST['word']; ?></textarea>
+			<textarea class="form-control" id="Hsource" rows="10" cols="86" onfocus="textarea_onfocus('hide')" onblur="textarea_onfocus('show')">請輸入你想要轉換的文字</textarea>
 		</div>
 	</div>
-</form>
-<div id="result"></div>
+	<div class="form-group text-center">
+		<button type="button" class="<?php echo get_btn(); ?>" name="convert" onclick="z()">轉換至拼音</button>
+		<button type="button" class="<?php echo get_btn(); ?>" name="convert" onclick="resetAll()">重置</button>
+	</div>
+	<div class="form-group">
+		<div class="col-sm-10 centerd">
+			<textarea class="form-control" id="Uresult" rows="10" cols="86"></textarea>
+		</div>
+	</div>
 <?php get_footer(); ?>
--- chinese-to-pinyin/script.js
+++ chinese-to-pinyin/script.js
@@ -1,11 +1,40 @@
-function get_pinyin(str){
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange=function(){
-		if (xmlhttp.readyState==4 && xmlhttp.status==200){
-			document.getElementById("result").innerHTML=xmlhttp.responseText;
+function pinyin(cc) {
+	var str = '';
+	var s;
+	for (var i = 0; i < cc.length; i++) {
+		if (icibahy.indexOf(cc.charAt(i)) != -1 && cc.charCodeAt(i) > 200) {
+			s = 1;
+			while (icibahy.charAt(icibahy.indexOf(cc.charAt(i)) + s) != ",") {
+				str += icibahy.charAt(icibahy.indexOf(cc.charAt(i)) + s);
+				s++;
+			}
+			str += " ";
+		} else {
+			str += cc.charAt(i);
 		}
 	}
-	xmlhttp.open("POST", "convert.php", true);
-	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	xmlhttp.send("word="+str);
+	return str;
+}
+function resetAll() {
+	document.getElementById('Hsource').value = "";
+	document.getElementById('Uresult').value = "";
+}
+function z() {
+	document.getElementById('Uresult').value = '';
+	var source = document.getElementById('Hsource').value;
+	var toPinyin = pinyin(source);
+	document.getElementById('Uresult').value = toPinyin;
+}
+
+function textarea_onfocus(mode){
+	var text = "請輸入你想要轉換的文字";
+	if(mode == "show"){
+		if(document.getElementById('Hsource').value == ""){
+			document.getElementById('Hsource').value = text;
+		}
+	}else{
+		if(document.getElementById('Hsource').value == text){
+			document.getElementById('Hsource').value = "";
+		}
+	}
 }
